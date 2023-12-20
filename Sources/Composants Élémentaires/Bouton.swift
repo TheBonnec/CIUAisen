12f6eb146808e222d7dd46ce7cc39bcb602bd669
TheBonnec <thomaslebonnec@icloud.com>
2023-12-20 14:28:46 +0100

    taille police bouton

--- Sources/Composants Élémentaires/Bouton.swift
+++ Sources/Composants Élémentaires/Bouton.swift
@@ -54,8 +54,10 @@ public struct Bouton: View {
             action()
         }, label: {
             Text(label)
+                .font(.system(size: 13))
                 .foregroundStyle(couleurDeFace)
                 .frame(height: 30)
+                //.padding(.vertical, 6)
                 .padding(.horizontal, 12)
                 .background(couleurDeFond)
                 .cornerRadius(4)
